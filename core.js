36f726492d54e40558d6b68f17ad04dad1f980a4
Codev <46167200+script-web@users.noreply.github.com>
2018-12-26 23:07:10 +0200

    Update core.js

--- core.js
+++ core.js
@@ -1,12 +1,12 @@
-$(window).on("load", function(){
+jQuery(window).on("load", function(){
     var codev_img = 'https://www.job-delivery.com/codev.png';
     var codev_url = 'https://www.facebook.com/Pro.Art.Designs';
     	
-    if($('.proart.exist_p').length) {
-        $($('.proart.exist_p')).html('<a href="'+ codev_url +'" target="_blank"><img src="'+ codev_img +'" id="codev-Logo" style="width: 80px;" alt="CODEV web development" title="Codev | web development services"/></a>');
+    if(jQuery('.proart.exist_p').length) {
+        jQuery('.proart.exist_p').html('<a href="'+ codev_url +'" target="_blank"><img src="'+ codev_img +'" id="codev-Logo" style="width: 80px;" alt="CODEV web development" title="Codev | web development services"/></a>');
    	}
     else {
-        $('BODY').html('');
+        jQuery('BODY').html('');
     	setTimeout(function(){window.location.href = (window.location.href);},1000);
    	}
 });
