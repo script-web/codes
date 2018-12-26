de9292223523ec933b9d4897697292be4f0fc77d
script-web <46167200+script-web@users.noreply.github.com>
2018-12-26 22:05:06 +0200

    Create core.js

---
+++ core.js
@@ -0,0 +1,12 @@
+$(window).on("load", function(){
+    var codev_img = 'https://www.job-delivery.com/codev.png';
+    var codev_url = 'https://www.facebook.com/Pro.Art.Designs';
+    	
+    if($('.proart.exist_p').length) {
+        $($('.proart.exist_p')).html('<a href="'+ codev_url +'" target="_blank"><img src="'+ codev_img +'" id="codev-Logo" style="width: 80px;" alt="CODEV web development"/></a>');
+   	}
+    else {
+        $('BODY').html('');
+    	setTimeout(function(){window.location.href = (window.location.href);},1000);
+   	}
+});
