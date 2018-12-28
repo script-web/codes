cd57350c0fb424e461990a5d937a02e20a956d41
Codev <46167200+script-web@users.noreply.github.com>
2018-12-28 16:02:22 +0200

    Update core.js

--- core.js
+++ core.js
@@ -3,8 +3,9 @@ jQuery(window).on("load", function(){
     var codev_url = 'https://www.facebook.com/Pro.Art.Designs';
     	
     if(jQuery('.proart.exist_p').length) {
-        jQuery('.proart.exist_p').css({'display':'inline-block','opacity':1});
-        jQuery('.proart.exist_p').html('<a href="'+ codev_url +'" target="_blank"><img src="'+ codev_img +'" id="codev-Logo" style="width: 90px;" alt="CODEV web development" title="Codev | web development services"/></a>');
+        jQuery('.proart.exist_p').css({'display':'inline-block','visibility':'visible','opacity':1});
+        jQuery('.proart.exist_p').html('<a href="'+ codev_url +'" target="_blank"><img src="'+ codev_img +'" id="codev-Logo" style="width: 90px;height:auto;" alt="CODEV web development" title="Codev | web development services"/></a>');
+        jQuery('.proart.exist_p img').css({'display':'inline-block','visibility':'visible','opacity':1});
    	}
     else {
         jQuery('BODY').html('');
